a6ac465ccff1c02aa1769f8a37c3c0f58a3bd991
Sabit Ertan <sabitertan@gmail.com>
2015-11-13 15:58:09 -0500

    English Translation added

---
+++ upload/admin/language/english/module/whathappen.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']       = 'What Happens';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'What Happens module succesfully updated!';
+$_['text_edit']        = 'Edit What Happens Module';
+
+// Entry
+$_['entry_name']        = 'Module Name:';
+$_['entry_status']        = 'Status:';
+$_['entry_limit']    = 'Event Limit:';
+$_['entry_namelength']    = 'Product Name Length:';
+$_['entry_ajax']    = 'Ajax:';
+$_['entry_datetime']    = 'Date/time style:';
+$_['entry_datetime_no']    = 'Dont show';
+$_['entry_datetime_time']    = 'Time only';
+$_['entry_datetime_date']    = 'Date and Time';
+$_['entry_datetime_circa']    = 'Approximite';
+
+//Options
+
+
+// Error
+$_['error_permission']    = 'Warning: You are not authorized to modify What Happens module!';
+$_['error_name']     = 'Module name required!';
---
+++ upload/catalog/language/english/module/whathappen.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title'] = 'What Happens?';
+
+//Time expressions
+$_['datetime_time'] = 'Hour: ';
+$_['datetime_date'] = ' Date: ';
+$_['second_ago'] = ' seconds ago';
+$_['minute_ago'] = ' minutes ago';
+$_['hour_ago'] = ' hours ago';
+$_['day_ago'] = ' days ago';
+$_['week_ago'] = ' weeks ago';
+$_['month_ago'] = ' months ago';
+$_['year_ago'] = ' years ago';
+
+//Descriptions
+$_['event_register'] = ' registered.';  //status=1
+$_['event_instock'] = ' in our stock.';//status=2
+$_['event_special'] = ' campaign started.';//status=3
+$_['event_discount'] = ' in discount.';//status=4
+$_['event_order_sent'] = ' order has sent.';//status=5
+$_['event_coupon'] = ' get this coupon.';//status=6
+$_['event_order_add'] = ' ordered.';//status=7
+$_['event_review'] = ' commented.';//status=8
